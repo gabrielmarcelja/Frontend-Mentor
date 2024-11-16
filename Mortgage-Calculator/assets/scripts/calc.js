dc846367062ebca7e7469b47088c65b767dc4262
Gabriel Marcelja <gabrielnitob@proton.me>
2024-11-16 02:12:19 -0300

    beta
    erro message - basic
    payment show - beta

--- Mortgage-Calculator/assets/scripts/calc.js
+++ Mortgage-Calculator/assets/scripts/calc.js
@@ -5,31 +5,77 @@ document.querySelector("#calc_btn").addEventListener("click", function(){
     let totalpayment = 0;
     let monthlyPayment = 0;
     let typeradio = false;
+    let totalMonths = 0;
+    let done = false;
     if(document.querySelector("input[name='typeradio']:checked")){
         typeradio = document.querySelector("input[name='typeradio']:checked").value;
     }
     if(iamount_inp > 0 && iterm_inp > 0 && irate > 0 && typeradio){
-    //    FAZER CALCULO E MOSTRAR
         if(typeradio == "radio_interest"){
-            // FOI SELECIONADO A OPÇÃO INTEREST ONLY
             irate = irate / 100;
             totalpayment = (iamount_inp * irate) * iterm_inp + iamount_inp;
-            let monthlyPayment = totalpayment / iterm_inp / 12;
-            console.log(monthlyPayment);
-            console.log(totalpayment)
-            // TOTALPAYMENT AGORA É O TOTAL A SE PAGAR
+            monthlyPayment = totalpayment / iterm_inp / 12;
+            // FORMATANDO NUMEROS
+            totalpayment = totalpayment.toLocaleString('eng', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            })
+            monthlyPayment = monthlyPayment.toLocaleString('eng', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            })
+            done = true;
         } else if (typeradio == "radio_rapay") {
-            // FOI SELECIONADO A OPÇÃO REPAYMENT
             irate = irate / 100 / 12;
-            let totalMonths = iterm_inp * 12;
+            totalMonths = iterm_inp * 12;
             monthlyPayment = iamount_inp * (irate * Math.pow(1 + irate, totalMonths)) / (Math.pow(1 + irate, totalMonths) - 1);
             totalpayment = monthlyPayment * totalMonths;
-            console.log(monthlyPayment);
-
-            console.log(totalpayment)
+            // FORMATANDO NUMEROS
+            totalpayment = totalpayment.toLocaleString('eng', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            })
+            monthlyPayment = monthlyPayment.toLocaleString('eng', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            })
+            done = true;
         }
+        if(done){
+            // ADICIONANDO VALORES NO HTML
+            document.querySelector("p.result_value_num").innerHTML = '€' + monthlyPayment;
+            document.querySelector("p.result_value_snum").innerHTML = '€' + totalpayment;
+            // MOSTRANDO ARTICLE
+            document.querySelector("article#result_cont").style.display = 'flex';
+            document.querySelector("div.result_main").style.display = 'none';
+            document.querySelector("div.result_show").style.display = 'unset';
+        }
+    }
+    if(!iamount_inp){
+        document.querySelector(".input_icon_first").style.background = `#D63326`;
+        document.querySelector(".input_icon_first").style.color = `white`;
     }else{
-        console.log("n")
-        // IDENTIFICAR ERRO E MOSTRAR
+        document.querySelector(".input_icon_first").style.background = `hsl(202, 86%, 94%)`;
+        document.querySelector(".input_icon_first").style.color = `hsl(202, 55%, 16%)`;
+    }
+
+    if(!iterm_inp){
+        document.querySelector("#input_iconfirst").style.background = `#D63326`;
+        document.querySelector("#input_iconfirst").style.color = `white`;
+    }else{
+        document.querySelector("#input_iconfirst").style.background = `hsl(202, 86%, 94%)`;
+        document.querySelector("#input_iconfirst").style.color = `hsl(202, 55%, 16%)`;
+    }
+
+    if(!irate){
+        document.querySelector("#input_iconttr").style.background = `#D63326`;
+        document.querySelector("#input_iconttr").style.color = `white`;
+    }else{
+        document.querySelector("#input_iconttr").style.background = `hsl(202, 86%, 94%)`;
+        document.querySelector("#input_iconttr").style.color = `hsl(202, 55%, 16%)`;
+    }
+
+    if(!typeradio){
+        // INPUT RADIO VAZIO
     }
 })  
