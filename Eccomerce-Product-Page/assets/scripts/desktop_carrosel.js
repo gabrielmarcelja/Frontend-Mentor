9b1abcf443f9db5a4fed48bbaa93dd42c7c7cf87
Gabriel Marcelja <gabrielnitob@proton.me>
2024-11-28 14:26:36 -0300

    desktop carrosel done basic

--- Eccomerce-Product-Page/assets/scripts/desktop_carrosel.js
+++ Eccomerce-Product-Page/assets/scripts/desktop_carrosel.js
@@ -4,7 +4,9 @@ let dom_image = document.querySelector("#desktop_imgdom");
 
 carrosel_img.forEach(img => {
     img.addEventListener("click", function(){
+        carrosel_img.forEach(item => {item.classList.remove("carrosel_select")});
         dom_image.src = img.src;
+        img.classList.add("carrosel_select");
     })
 })
 }
